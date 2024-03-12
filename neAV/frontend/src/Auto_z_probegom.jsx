b1308e68f8a9447ff95ccb389c3effc7d7fdf375
Viktoryia Grib <130666532+Vika-Grib@users.noreply.github.com>
2024-03-12 15:58:04 +0300

    фронт + парсинг объявлений + выгрузка их на сайт

--- neAV/frontend/src/Auto_z_probegom.jsx
+++ neAV/frontend/src/Auto_z_probegom.jsx
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 
 const CarAdvertList = () => {
   const [adverts, setAdverts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Добавляем состояние для текущей страницы
 
   useEffect(() => {
     const fetchAdverts = async () => {
       try {
-        const response = await axios.get('/api/v1/mainapp/car/used/');
+        // Используем текущую страницу в запросе к API
+        const response = await axios.get(`/api/v1/mainapp/car/used/?page=${currentPage}`);
         setAdverts(response.data.results);
       } catch (error) {
         console.error('Ошибка при получении списка объявлений', error);
@@ -17,7 +19,16 @@ const CarAdvertList = () => {
     };
 
     fetchAdverts();
-  }, []);
+  }, [currentPage]); // Добавляем currentPage в список зависимостей useEffect
+
+  // Функции для навигации
+  const goToPreviousPage = () => {
+    setCurrentPage(currentPage > 1 ? currentPage - 1 : 1);
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   if (!adverts.length) {
     return <div>Загрузка списка объявлений...</div>;
@@ -39,13 +50,23 @@ const CarAdvertList = () => {
                 <br />
               </div>
             )}
-            <p style={infoStyle}>{advert.name}</p>
-            <p style={infoStyle}>Модель авто: {advert.model}</p>
+            <p style={{ ...infoStyle, fontWeight: 'bold' }}>{advert.name}</p>
+            <p style={infoStyle}>Цена: {advert.price_for_bel_rub} BYN </p>
+            <p style={infoStyle}>Кузов авто: {advert.kyzov}</p>
+            <p style={infoStyle}>Привод: {advert.privod}</p>
             <p style={infoStyle}>Цвет авто: {advert.color}</p>
+            <p style={infoStyle}>Пробег авто: {advert.probeg}</p>
 
             <Link to={`/api/v1/mainapp/car/advert/${advert.id}`} style={linkStyle}>Подробнее</Link>
           </div>
         ))}
+      </div>
+      <div>
+        {/* Кнопки для навигации */}
+        {currentPage > 1 && (
+          <button onClick={goToPreviousPage}>Предыдущая страница </button>
+        )}
+        <button onClick={goToNextPage}>Следующая страница</button>
       </div>
     </div>
   );
